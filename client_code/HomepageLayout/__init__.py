a5ba318ca073f5f84fea62fd55538d0fb8101e8d
myalexty@gmail.com <myalexty@gmail.com>
2026-05-04 11:59:43 +0000

    Edited HomepageLayout

--- client_code/HomepageLayout/__init__.py
+++ client_code/HomepageLayout/__init__.py
@@ -15,5 +15,6 @@ class HomepageLayout(HomepageLayoutTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
+    self.layout.nav_visible = False
     # Any code you write here will run before the form opens.
+    
